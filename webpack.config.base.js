5b9cbac3f7adc70f34927387498e5c2fadf3d078
Lucy Orbach <lucyorbach@Lucys-MBP-4009.fios-router.home>
2019-03-04 01:35:54 -0500

    adds html-webpack-plugin and webpack-merge. creates diff webpack configurations fordev and prod

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,12 +1,13 @@
 //node's path module
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // modifies default configuration for webpack
 
 module.exports = {
 	// modes: development || production
 	// we can modified the mode on cli using flags: npm run build -- --mode development
-	mode: 'development',
+	// mode: 'development',
 	// path to entry file (first one to be read). default: './src/index.js'
 	entry: './src/index.js',
 	// bundled files configuration...
@@ -15,7 +16,7 @@ module.exports = {
 		path:path.join(__dirname, 'dist'),
 		// name of bundle file on output
 		filename:  'app.bundle.js'
-	}, // result .dist/app.bundle.js
+	}, // results in -> .dist/app.bundle.js
 	module: {
 		rules: [
 			{
@@ -27,5 +28,8 @@ module.exports = {
 				}
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: './src/index.html' })
+	]
 };
